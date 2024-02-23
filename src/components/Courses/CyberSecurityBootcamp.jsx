0fab12e5b2fa436e05aa83896533a6925fc3363c
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-02-23 13:35:49 +0600

    Added: created CyberSecurityBootcamp page

---
+++ src/components/Courses/CyberSecurityBootcamp.jsx
@@ -0,0 +1,10 @@
+import { Title } from '../../shared';
+
+export default function CyberSecurityBootcamp() {
+	return (
+		<>
+			<Title>Courses | Cyber Security Bootcamp</Title>
+			<section>CyberSecurityBootcamp</section>
+		</>
+	);
+}
---
+++ src/pages/Courses.jsx
@@ -0,0 +1,10 @@
+import { Title } from '../shared';
+
+export default function Courses() {
+	return (
+		<section>
+			<Title>Courses</Title>
+			<h1>Courses</h1>
+		</section>
+	);
+}
