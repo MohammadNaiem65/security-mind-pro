186c04e94782e6e7514cc4b229823bc7e26c3255
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-02-25 08:35:03 +0600

    Added: added some resource image

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,1 +1,4 @@
 export { default as logo } from './logo.png';
+export { default as coursePageBg } from './course-page-background.png';
+export { default as cyberSecurityCourseImg } from './cyber-security-course.jpg';
+export { default as mashihoorRahmanImg } from './mashihur-rahman.jpeg';
