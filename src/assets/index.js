c9d0c5d97b9777b9c8b0512d247d674fbcb8f75e
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-02-25 08:39:48 +0600

    Solved: corrected the image names

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
 export { default as logo } from './logo.png';
-export { default as coursePageBg } from './course-page-background.png';
-export { default as cyberSecurityCourseImg } from './cyber-security-course.jpg';
+export { default as coursePageBg } from './course-page-background.jpg';
+export { default as cyberSecurityCourseImg } from './cyber-security-course.png';
 export { default as mashihoorRahmanImg } from './mashihur-rahman.jpeg';
